30931b2574535095b5da7f378a6d433df7f017b1
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-08-11 00:08:59 +0800

    Add API providers

---
+++ src/api/consumet-api.ts
@@ -0,0 +1,84 @@
+import { AnimeAdvancedSearchParams, AnimeProviders } from "@/lib/types";
+
+const anilistBase = `${process.env.CONSUMET_API_BASE_URL}/meta/anilist`;
+
+function createURL(
+  base: string,
+  path: string,
+  params: Record<string, string | number | string[] | boolean | undefined>
+) {
+  const url = new URL(`${base}/${path}`);
+  Object.entries(params).forEach(([key, value]) => {
+    if (value === undefined) {
+      return;
+    }
+    if (key === "perPage" && Number(value) > 40) {
+      value = 40;
+    }
+    if (typeof value === "boolean") {
+      value = value.toString();
+    }
+    if (Array.isArray(value)) {
+      const formattedValue = `[${value.map((v) => `"${value}"`).join(",")}]`;
+      url.searchParams.append(key, formattedValue);
+    } else {
+      url.searchParams.append(key, String(value));
+    }
+  });
+  return url.toString();
+}
+
+export const animeAPIQuery = {
+  meta: {
+    anilist: {
+      search: (params: AnimeAdvancedSearchParams) =>
+        createURL(anilistBase, `advanced-search`, params),
+
+      data: ({ id }: { id: string }) =>
+        createURL(anilistBase, `data/${id}`, {}),
+
+      info: ({ id }: { id: string }) =>
+        createURL(anilistBase, `info/${id}`, {}),
+
+      episodes: ({
+        id,
+        ...params
+      }: {
+        id: string;
+        provider?: AnimeProviders;
+        fetchFiller?: string | boolean;
+        dub?: string | boolean;
+        locale?: string;
+      }) => createURL(anilistBase, `episodes/${id}`, params),
+
+      watch: ({
+        episodeId,
+        ...params
+      }: {
+        episodeId: string;
+        provider: AnimeProviders;
+      }) => createURL(anilistBase, `watch/${episodeId}`, params),
+
+      trending: (params: { page?: number; perPage?: number }) =>
+        createURL(anilistBase, `trending`, params),
+
+      popular: (params: { page?: number; perPage?: number }) =>
+        createURL(anilistBase, `popular`, params),
+
+      airingSchedule: (params: { page?: number; perPage?: number }) =>
+        createURL(anilistBase, `airing-schedule`, params),
+
+      recentEpisodes: (params: {
+        page?: number;
+        perPage?: number;
+        provider?: AnimeProviders;
+      }) => createURL(anilistBase, `recent-episodes`, params),
+
+      character: ({ id, ...params }: { id: string }) =>
+        createURL(anilistBase, `character/${id}`, params),
+
+      genre: (params: { genres: string[]; page?: number; perPage?: number }) =>
+        createURL(anilistBase, `genre`, params),
+    },
+  },
+};
--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -1,4 +1,13 @@
-import { Icons } from "@/components/ui/Icons";
+import { Icons } from "../components/ui/icons";
+
+import {
+  ASFormatArray,
+  ASGenresArray,
+  ASSeasonArray,
+  ASSortArray,
+  ASStatusArray,
+  ASTypeArray,
+} from "./constants";
 
 export type NavItem = {
   title: string;
@@ -14,3 +23,48 @@ export type NavItemWithChildren = NavItem & {
 };
 
 export type MainNavItem = NavItem & {};
+
+// Start: Consumet API types
+
+export type ASType = (typeof ASTypeArray)[number];
+export type ASSeason = (typeof ASSeasonArray)[number];
+export type ASFormat = (typeof ASFormatArray)[number];
+export type ASSort = (typeof ASSortArray)[number];
+export type ASGenres = (typeof ASGenresArray)[number];
+export type ASStatus = (typeof ASStatusArray)[number];
+
+export type AnimeProviders = "gogoanime" | "zoro";
+
+export type AnimeAdvancedSearchParams = {
+  query?: string;
+  page?: number;
+  perPage?: number;
+  type?: ASType;
+  genres?: ASGenres[];
+  id?: string;
+  format?: ASFormat;
+  sort?: ASSort[];
+  status?: ASStatus;
+  year?: number;
+  season?: ASSeason;
+};
+
+// End: Consumet API types
+
+// Start: Aniwatch API types
+
+export type AniwatchSearchParams = {
+  q: string;
+  page?: number;
+  genres?: string;
+  type?: string;
+  season?: string;
+  language?: string;
+  status?: string;
+  rated?: string;
+  start_date?: string;
+  end_date?: string;
+  score?: string;
+};
+
+// End: Consumet API types
